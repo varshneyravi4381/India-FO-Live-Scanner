83aed8547bf626c6a69cbf9e420b7d92ccda9823
varshneyravi4381 <varshneyravi4381@gmail.com>
2026-08-25 11:56:11 +0530

    Add files via upload

---
+++ static/service-worker.js
@@ -0,0 +1,1 @@
+self.addEventListener("install",e=>self.skipWaiting());self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.map(x=>caches.delete(x)))).then(()=>self.registration.unregister())));self.addEventListener("fetch",e=>e.respondWith(fetch(e.request)));
